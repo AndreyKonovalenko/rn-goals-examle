ffa16f8083dae11f74bb577ff0f637ac8be25128
AndreyKonovalenko <andreinew@mail.ru>
2019-08-15 20:42:56 +0300

    testing inline styling concept

--- App.js
+++ App.js
@@ -4,13 +4,22 @@ import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
 export default function App() {
   return (
     <View style={{ padding: 80 }}>
-      <View>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}
+      >
         <TextInput
           placeholder='Course Goal'
-          style={{ borderColor: 'black', borderWidth: 1, padding: 10 }}
+          style={{
+            borderColor: 'black',
+            borderWidth: 1,
+            padding: 10,
+            width: '80%'
+          }}
         />
-      </View>
-      <View>
         <Button title='Add' />
       </View>
     </View>
