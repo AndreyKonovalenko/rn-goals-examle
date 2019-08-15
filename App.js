d95375d4154135e45a0a12b7c0544ff55d0d5861
AndreyKonovalenko <andreinew@mail.ru>
2019-08-15 21:10:48 +0300

    Added styleSheet object indstead of inline styling

--- App.js
+++ App.js
@@ -3,25 +3,28 @@ import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ padding: 80 }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center'
-        }}
-      >
-        <TextInput
-          placeholder='Course Goal'
-          style={{
-            borderColor: 'black',
-            borderWidth: 1,
-            padding: 10,
-            width: '80%'
-          }}
-        />
+    <View style={styles.screen}>
+      <View style={styles.inputConteiner}>
+        <TextInput placeholder='Course Goal' style={styles.input} />
         <Button title='Add' />
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 80
+  },
+  inputConteiner: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    width: '80%'
+  }
+});
